c44b6eaa780969e6b79367ca02a9166a6f93bf90
aakashsaini09 <aakashsaini948585@gmail.com>
2025-09-09 12:43:59 +0530

    clerk done

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div className=''>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default page
