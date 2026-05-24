de6362ebfbe4e8208bc78ba436ce874bfdc1dc2d
aakashsaini09 <aakashsaini948585@gmail.com>
2026-05-24 23:10:52 +0530

    popup for room creation

--- backend/src/models/room.model.js
+++ backend/src/models/room.model.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import {Schema} from 'mongoose'
 const roomSchema = new Schema({
+    _id: ObjectId,
     title: String,
     language: String,
-    creatorId: Date,
-    isOccupied: String,
+    creatorId: String,
+    createdAt: Date,
+    activeParticipants: Number,
     lastActiveAt: Date
 },{timeseries, timestamps})
 
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,18 +2,25 @@
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { useState } from 'react'
+import { Button } from './ui/button'
 const Navbar = () => {
     const [popup, setpopup] = useState(false);
     function changePopup(){
         setpopup(pre => !pre)
     }
+    // console.log("userprofile data: ", UserProfile)
     return (
         <>
             <div className='bg-black'>
@@ -29,17 +36,36 @@ const Navbar = () => {
                     <div className='w-full border-b-2 border-white flex justify-around'>
                         <button className='bg-gray-600 py-2 px-3 cursor-pointer rounded-sm' onClick={changePopup}>Create room</button>
                         {popup ? (<Dialog open={popup} onOpenChange={setpopup}>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
+                            <form action="">
+                                <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </Field>
+            <Field>
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+                            </form>
 </Dialog>): (<></>)}
                         <UserButton />
+                        {/* <UserProfile/> */}
                     </div>
                 </Show>
             </div>
