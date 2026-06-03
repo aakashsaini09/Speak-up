58243ed74b58c59d4ef8fe145484e6c011202942
aakashsaini09 <aakashsaini948585@gmail.com>
2026-06-03 18:14:08 +0530

    world chat UI

--- frontend/components/HomePage.tsx
+++ frontend/components/HomePage.tsx
@@ -1,52 +1,36 @@
 "use client"
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { toast } from 'sonner';
-import RoomCard from './RoomCard';
-import Navbar from './Navbar';
 
+import { useState } from 'react';
+import Navbar from './Navbar';
+import {
+    ToggleGroup,
+    ToggleGroupItem,
+} from "@/components/ui/toggle-group"
+import GetRooms from './GetRooms';
+import WorldChat from './WorldChat';
 const HomePage = () => {
-    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
-    const [rooms, setRooms] = useState<any[]>([])
-    useEffect(() => {
-        fetchRoomFunction()
-    }, [])
-    const fetchRoomFunction = async () => {
-        try {
-            const res = await axios.get(`${backendUrl}/api/room`);
-            console.log("response is: ", res.data.rooms)
-            setRooms(res.data.rooms)
-            if (res.data?.success) {
-                toast.success("Room fetched successfully!");
-            } else {
-                toast.error("Something went wrong");
-            }
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.log(
-                    "Response Data:",
-                    error.response?.data
-                );
-                console.log("Full Response:", error.response);
-                toast.error(
-                    error.response?.data?.message ||
-                    "Request failed"
-                );
-            } else {
-                console.log(error);
-                toast.error("Unknown error");
-            }
-        }
-    }
+
+    const [value, setValue] = useState<string | undefined>("rooms")
+
     return (
         <div>
-            <Navbar/>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-zinc-950 h-screen px-32 py-12">
-                    {rooms && rooms.map((room, idx) => (
-                        // <div key={idx}>{typeof room === 'object' ? JSON.stringify(room) : String(room)}</div>
-                        <div key={idx}><RoomCard room={room}/></div>
-                    ))}
-                </div>
+            <Navbar />
+            {value == 'rooms' ? (
+                <GetRooms/>
+                ) : (
+                    <WorldChat/>
+                )}
+                    <div className='fixed w-full flex bottom-2 justify-around'>
+                <ToggleGroup variant="outline" type="single" value={value} onValueChange={(value) => { if (value) setValue(value) }} defaultValue="rooms">
+                    <ToggleGroupItem value="rooms" aria-label="Toggle all" className={`px-4 py-2 ${value == 'rooms' ? 'text-black hover:text-black hover:bg-black' : 'text-white hover:text-black'}`}>
+                        Rooms
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="chat" aria-label="Toggle missed" className={`px-4 py-2 ${value == 'chat' ? 'text-black hover:text-black hover:bg-black' : 'text-white hover:text-black'}`}>
+                        World-Chat
+                    </ToggleGroupItem>
+                </ToggleGroup>
+            </div>
+            
         </div>
     )
 }
---
+++ frontend/components/WorldChat.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorldChat = () => {
+  return (
+    <div>
+      WorldChat
+    </div>
+  )
+}
+
+export default WorldChat
