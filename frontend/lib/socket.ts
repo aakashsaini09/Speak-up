defde8b0f350fb886fed2b7d17723323c8054cc5
aakashsaini09 <aakashsaini948585@gmail.com>
2026-06-15 18:51:29 +0530

    prod bug

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,5 +1,12 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  "http://localhost:3001"
-);
+const SOCKET_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export const socket =
+  io(SOCKET_URL!, {
+    transports: [
+      "websocket",
+      "polling"
+    ]
+  });
