47715cc472d1541646c7bb1b6c2df876a9dc1fa0
aakashsaini09 <aakashsaini948585@gmail.com>
2026-07-18 18:17:11 +0530

    stun to turn server

--- frontend/app/room/[id]/page.tsx
+++ frontend/app/room/[id]/page.tsx
@@ -41,10 +41,19 @@ type FriendRelations = Record<string, FriendRelation>;
 
 const NONE_RELATION: FriendRelation = { status: "none" };
  
-const RTC_CONFIG: RTCConfiguration = {
+const RTC_CONFIG = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
+    {
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+      ],
+    },
+    {
+      urls: "turn:turn.speak-up.online:3478",
+      username: "speakup",
+      credential: "testing123",
+    },
   ],
 };
  
